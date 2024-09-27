f52af35ee478440add530f0e624d537f2baa1aff
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-09-26 23:42:03 -0400

    Created a rudimentary bootloader that prints X

---
+++ bl.asm
@@ -0,0 +1,9 @@
+bits 16
+
+mov AH, 0x0e		; Teletype screen
+mov AL, 0x58		; Put the letter 'x'
+INT 10h
+jmp $			; Infinite loop
+
+times 510-($-$$) db 0	; Pad the file with zeros
+dw 0xAA55		; Put the magic number
