955fdf083daf1223e44709f5cdf995510acd2ac6
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-09-27 15:28:55 -0400

    Project Part 3 - 8 - Called Interrupt

--- bl.asm
+++ bl.asm
@@ -2,33 +2,78 @@
 [org 0x7c00]			; Task 1: Set 16-bit mode and origin.
 
 ; Entry point
-mov AH, 0x0e			; Teletype screen
-mov AL, 0x58			; Put the letter 'x'
-INT 10h
+main:
+	mov AH, 0x0e			; Teletype screen
+	mov AL, 0x58
+	INT 10h
 
-mov AL, 0x0a
-INT 10h
+	call new_line
 
-mov [DRIVE_NUM], DL
-mov AL, [DRIVE_NUM]
-INT 10h
+	mov [DRIVE_NUM], DL
+	mov AL, [DRIVE_NUM]
+	INT 10h
 
-mov bp, 0x9000			; Task 4: Set up the stack
-mov sp, bp
+	call new_line	
 
-call disk_test			; Task 5: Call disk_test
+	mov BX, hello_world
+	call print_string
 
-jmp $				; Infinite loop
+	call new_line
+
+	mov bp, 0x9000			; Task 4: Set up the stack
+	mov sp, bp
+
+	call disk_test			; Task 5: Call disk_test
+
+	jmp $				; Infinite loop
+
+new_line:
+	mov AH, 0x0e
+	mov AL, 0x0a
+	INT 10h
+
+	mov AL, 0x0d
+	INT 10h
+
+	ret
+
+disk_test:
+	pusha			; Task 6: Push all registers onto the stack
+	
+	mov AH, 0x02		; Task 7: Set up registers properly
+	mov AL, 0x01
+
+	mov BX, [KERNEL_ADDRESS]
+	
+	mov CH, 0x00
+	mov CL, 0x02
+
+	mov DH, 0x00
+	mov DL, [DRIVE_NUM]
+
+	INT 13h			; Task 8: Call Interrupt
+
+print_string:
+	pusha
+	mov AH, 0x0e
+print_string_lp:
+	mov AL, [bx]
+	cmp AL, 0
+	je print_done
+	int 10h
+	add BX, 1		; Increment BX
+	jmp print_string_lp
+print_done:
+	popa
+	ret
 
 ; data
-KERN_ADDRESS 	dw 0x1000	; Task 2: Store kernel address
+KERNEL_ADDRESS 	dw 0x1000	; Task 2: Store kernel address
 DRIVE_NUM	db 0		; Task 3: Inspect the value printed.
 				; Should be 0x80
 
 hello_world db 'Hello World', 0
 
-disk_test:
-	pusha			; Task 6: Push all registers onto the stack
 
 ; Pad file with zeroes
 times 510-($-$$) db 0	
