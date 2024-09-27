f64ed0662bb97d52355891ee2b92c25ba04a7db7
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-09-27 15:02:21 -0400

    Project Part 3 - 2 - Store Kernel Address

--- bl.asm
+++ bl.asm
@@ -1,10 +1,20 @@
-bits 16
-org 0x7c00
+[bits 16]
+[org 0x7c00]			; Task 1: Set 16-bit mode and origin.
 
-mov AH, 0x0e		; Teletype screen
-mov AL, 0x58		; Put the letter 'x'
+; Entry point
+mov AH, 0x0e			; Teletype screen
+mov AL, 0x58			; Put the letter 'x'
 INT 10h
-jmp $			; Infinite loop
+jmp $				; Infinite loop
 
-times 510-($-$$) db 0	; Pad the file with zeros
-dw 0xAA55		; Put the magic number
+; data
+KERN_ADDRESS 	dw 0x1000	; Task 2: Store kernel address
+
+hello_world db 'Hello World', 0
+
+; Pad file with zeroes
+times 510-($-$$) db 0	
+
+; Magic Number
+db 0x55
+db 0xaa
