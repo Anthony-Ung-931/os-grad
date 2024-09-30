2592c9ac3f20056fa76bd80407c853765ec31946
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-09-30 14:10:35 -0400

    Completed up to task 11.

--- bl.asm
+++ bl.asm
@@ -20,12 +20,65 @@ main:
 
 	call new_line
 
-	mov bp, 0x9000			; Task 4: Set up the stack
-	mov sp, bp
+	mov BP, 0x9000			; Task 4: Set up the stack
+	mov SP, BP
 
 	call disk_test			; Task 5: Call disk_test
 
 	call idle_permanently
+
+
+disk_test:
+	pusha			; Task 6: Push all registers onto the stack
+				; Task 7: Set up registers properly
+	mov AH, 0x02		; 0x02: Mode - Read sectors
+	mov AL, NUM_SECTORS	; Number of sectors
+	mov BX, KERNEL_ADDRESS	; BX - Offset
+	mov CH, 0x00		; CH - Cylinder 0
+	mov CL, 0x01		; CL - Sector 1
+				; 	Sector 1 contains the bootloader
+				;	Sectors 2+ contain the kernel
+	mov DH, 0x00		; DH - Head 0
+	mov DL, [DRIVE_NUM]	; DL - Drive
+
+	INT 13h			; Task 8: Call Interrupt
+	
+	jc ERROR_disk_test	; Task 9: Query interrupt flag.
+				; If carry flag set, display error.
+disk_test_sectors:
+	cmp AL, NUM_SECTORS	; Task 10: Check number of sectors read
+	jne ERROR_disk_bad_read
+
+	popa			; Task 11: Pop all registers and return
+	ret
+
+ERROR_disk_test:
+				; Task 9: Print error message 
+				; 		if disk read failed
+	mov bx, err_msg_disk_fail 
+	call print_string
+	call new_line
+	call idle_permanently
+
+ERROR_disk_bad_read:
+	mov bx, err_msg_disk_bad_read 
+	call print_string
+	call new_line
+	call idle_permanently
+
+print_string:
+	pusha
+	mov AH, 0x0e
+print_string_lp:
+	mov AL, [BX]
+	cmp AL, 0
+	je print_done
+	INT 10h
+	add BX, 1		; Increment BX
+	jmp print_string_lp
+print_done:
+	popa
+	ret
 
 new_line:
 	mov AH, 0x0e
@@ -37,46 +90,18 @@ new_line:
 
 	ret
 
-disk_test:
-	pusha			; Task 6: Push all registers onto the stack
-	
-	mov AH, 0x02		; Task 7: Set up registers properly
-	mov AL, 0x01
-
-	mov BX, [KERNEL_ADDRESS]
-	
-	mov CH, 0x00
-	mov CL, 0x02
-
-	mov DH, 0x00
-	mov DL, [DRIVE_NUM]
-
-	INT 13h			; Task 8: Call Interrupt
-
-print_string:
-	pusha
-	mov AH, 0x0e
-print_string_lp:
-	mov AL, [bx]
-	cmp AL, 0
-	je print_done
-	int 10h
-	add BX, 1		; Increment BX
-	jmp print_string_lp
-print_done:
-	popa
-	ret
-
 idle_permanently:
 	jmp $			; Infinite Loop
 
 ; data
-KERNEL_ADDRESS 	dw 0x1000	; Task 2: Store kernel address
-DRIVE_NUM	db 0		; Task 3: Inspect the value printed.
+KERNEL_ADDRESS 	equ 0x1000	; Task 2: Store kernel address
+DRIVE_NUM	equ 0		; Task 3: Inspect the value printed.
 				; Should be 0x80
+NUM_SECTORS 	equ 0x01
 
 hello_world db 'Hello World', 0
-
+err_msg_disk_fail db 'ERROR: Disk cannot be read.', 0
+err_msg_disk_bad_read db 'ERROR: Disk can be read but the number of sectors is wrong', 0
 
 ; Pad file with zeroes
 times 510-($-$$) db 0	
