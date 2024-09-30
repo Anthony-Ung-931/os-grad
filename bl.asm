85dde22d0c992e1e6ca503cd7eb6b93c4714d284
Anthony-Ung-931 <123090218+Anthony-Ung-931@users.noreply.github.com>
2024-09-30 15:24:01 -0400

    Restoring bl.asm to last commit

--- bl.asm
+++ bl.asm
@@ -3,6 +3,23 @@
 
 ; Entry point
 main:
+	mov AH, 0x0e			; Teletype screen
+	mov AL, 0x58
+	INT 10h
+
+	call new_line
+
+	mov [DRIVE_NUM], DL
+	mov AL, [DRIVE_NUM]
+	INT 10h
+
+	call new_line	
+
+	mov BX, hello_world
+	call print_string
+
+	call new_line
+
 	mov bp, 0x9000
 	mov sp, bp
 
