f92d8ff8b933663fe82ec85a5449134f2d5cddba
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-10-24 13:01:21 -0400

    Added a new string to test printing strings with newline characters

--- kernel.c
+++ kernel.c
@@ -10,12 +10,18 @@ int main() {
 	char* hello = "Hello World";
 	char* test_str = "Called after print_line()";
 
+	char* evil_string = "Hello World \\n\nAgain";
+
 	clear_terminal();
 	print_string(hello);
 	print_character('\n');
 
 	print_line(hello);
 	print_string(test_str);
+
+	print_character('\n');
+
+	print_string(evil_string);
 
 	if(test_print_overflow_flag) {
 		test_print_overflow();
