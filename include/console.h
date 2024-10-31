03a48731a4ac7602523f2180b98c09327c132653
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-10-30 22:19:40 -0400

    Fixed function prototypes

--- include/console.h
+++ include/console.h
@@ -1,15 +1,4 @@
 
-void clear_terminal();
-void print_character(char c);
-void print_string(char* str);
-void print_line(char* str);
-
-void set_terminal_font_color(VGA_Color);
-void set_terminal_background_color(VGA_Color);
-void print_character_with_color(char c, VGA_Color);
-void print_string_with_color(char* str, VGA_Color);
-void print_line_with_color(char*, VGA_Color);
-
 typedef enum {
 	BLACK = 0,
 	BLUE,
@@ -28,3 +17,15 @@ typedef enum {
 	YELLOW,
 	WHITE
 } VGA_Color;
+
+void clear_terminal();
+void print_character(char c);
+void print_string(char* str);
+void print_line(char* str);
+
+void set_terminal_font_color(VGA_Color color);
+void set_terminal_background_color(VGA_Color color);
+void print_character_with_color(char c, VGA_Color color);
+void print_string_with_color(char* str, VGA_Color color);
+void print_line_with_color(char*, VGA_Color color);
+
