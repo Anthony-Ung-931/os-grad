f3770477eea99f83f73136316ad57aedd99a77fd
Anthony-Ung-931 <123090218+Anthony-Ung-931@users.noreply.github.com>
2024-10-30 23:01:31 -0400

    Put McKee's test code

--- kernel.c
+++ kernel.c
@@ -10,6 +10,61 @@ const int flag_test_preliminary_colors = 0;
 void run_tests();
 
 int main() {
+	char* str1= " HELLO ";
+	char* str2 = " WORLD ";
+	char* str3 = " TODAY ";
+	
+	print_line(str1);
+	set_terminal_font_color(BLUE);
+	print_line(str2);
+	set_terminal_font_color(YELLOW);
+	print_line(str3);
+	print_character_with_color('A', GREEN);
+	print_string_with_color(" BIG STRING", CYAN);
+	print_line_with_color("A new line", BROWN);
+	print_line_with_color(" is here", RED);
+
+	print_line(str1);
+	set_terminal_font_color(BLUE);
+	print_line(str2);
+	set_terminal_font_color(YELLOW);
+	print_line(str3);
+	print_character_with_color('A', GREEN);
+	print_string_with_color(" BIG STRING", CYAN);
+	print_line_with_color("A new line", BROWN);
+	print_line_with_color(" is here", RED);
+	
+	print_line(str1);
+	set_terminal_font_color(BLUE);
+	print_line(str2);
+	set_terminal_font_color(YELLOW);
+	print_line(str3);
+	print_character_with_color('A', GREEN);
+	print_string_with_color(" BIG STRING", CYAN);
+	print_line_with_color("A new line", BROWN);
+	print_line_with_color(" is here", RED);
+
+	print_line(str1);
+	set_terminal_font_color(BLUE);
+	print_line(str2);
+	set_terminal_font_color(YELLOW);
+	print_line(str3);
+	print_character_with_color('A', GREEN);
+	print_string_with_color(" BIG STRING", CYAN);
+	print_line_with_color("A new line", BROWN);
+	print_line_with_color(" is here", RED);
+	
+	print_line(str1);
+	set_terminal_font_color(BLUE);
+	print_line(str2);
+	set_terminal_font_color(YELLOW);
+	print_line(str3);
+	print_character_with_color('A', GREEN);
+	print_string_with_color(" BIG STRING", CYAN);
+	print_line_with_color("A new line", BROWN);
+	
+	print_line_with_color(" is here", RED);
+	
 	run_tests();
 	while(1);
 }
@@ -18,9 +73,7 @@ void run_tests() {
 	if(flag_test_print_overflow != 0) {
 		test_print_overflow();
 	}
-
-	if(flag_test_preliminary_colors != 0) {
+	else if(flag_test_preliminary_colors != 0) {
 		test_preliminary_colors();
 	}
-
 }
