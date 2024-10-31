7a691919b33606fa43835557579ffe1ff871801d
Anthony-Ung-931 <123090218+Anthony-Ung-931@users.noreply.github.com>
2024-10-30 22:42:23 -0400

    Update tests.c to include another internal test

--- tests/tests.c
+++ tests/tests.c
@@ -5,3 +5,27 @@ void test_print_overflow() {
 		print_character('\n');
 	}
 }
+
+void test_preliminary_colors() {
+	set_terminal_font_color(CYAN);
+	
+	char* hello = "Hello World";
+	char* test_str = "Called after print_line()";
+
+	char* evil_string = "Hello World \\n\nAgain";
+
+	clear_terminal();
+	print_string(hello);
+	print_character('\n');
+
+	print_line(hello);
+	print_string(test_str);
+
+	print_character('\n');
+
+	print_string(evil_string);
+
+	if(test_print_overflow_flag) {
+		test_print_overflow();
+	}
+}
