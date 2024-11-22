9e87c5ca825066bbd06985336e764bca5d1b4d42
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-11-21 20:47:51 -0500

    Function for bad command is implemented.

--- commands/commands.c
+++ commands/commands.c
@@ -2,9 +2,15 @@
 #include "dios_strings.h"
 
 const char* HELP = "help";
+char* parsed_command;
+
+/**
+ * Only one argument is necessary for the color commands.
+ */
+char* tokens[2];
 
 void help();
-
+void bad_command();
 
 /**
  * Most commands are small enough that I can provide them in this .c file.
@@ -15,8 +21,16 @@ void eval_command(char* command) {
 	if(strcmp(parsed_command, HELP) == 0) {
 		help();	
 	}
+	else {
+		bad_command();
+	}
 }
 
 void help() {
 	print_line("List of Commands");
+	print_line("    exit");
+}
+
+void bad_command() {
+	print_line("Bad command. Type 'help' for a list of all commands.");
 }
